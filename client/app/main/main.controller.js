2a943b73d72487aced44da9223cfad9e224fa073
ECMachado <wam-development@wam-development.(none)>
2014-10-31 12:13:02 +0000

    Added video

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,33 @@
 'use strict';
 
 angular.module('prototypeApp')
-  .controller('MainCtrl', function ($scope, $http) {
+	.controller('MainCtrl', function ($scope, $http, $sce) {
+		$scope.modal = false;
 
-  });
+		$scope.config = {
+					sources: [
+						{src: $sce.trustAsResourceUrl('assets/video.mp4'), type: 'video/mp4'}
+					],
+					theme: {
+						url: "styles/themes/default/videogular.css"
+					},
+					autoPlay: true
+				};
+
+		$scope.toggleVideo = function () {
+			if ($scope.modal === true) {
+				$('#video').children().remove();
+				$scope.modal = false;
+			}
+			else {
+				$scope.modal = true;
+
+				var instance = $('#video').vide({mp4: 'assets/video.mp4'},
+						{
+							muted: false,
+							posterType: 'none'
+						}
+					);
+			}
+		};	
+	});
